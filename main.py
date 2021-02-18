ae39a093367aa12cfb244666079af0e8593c7271
saleem <saleemdev2020@gmail.com>
2021-02-18 20:52:32 +0530

    18 feb

--- main.py
+++ main.py
@@ -8,7 +8,21 @@ else:
 #message end hear
 try:
     x=jksdfhb
+    print(hi)
 except:
     print("ok")
+
+#message end hear
+
+x=10
+
+if x==20:
+    print("ok")
+elif x<10:
+    print("nagative")
+elif x==10:
+    print("positive")
+elif x<20:
+    print("true")
 
 
